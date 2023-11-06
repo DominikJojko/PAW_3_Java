ff7cd3dcbfc358c4ce9d770174a4bdca46d92c08
Dominik Jojko <128039056+DominikJojko@users.noreply.github.com>
2023-11-06 16:38:14 +0100

    jsf_05_szablonowanie

---
+++ jsf_05_szablonowanie/src/main/java/com/jsfcourse/calc/jsf01.java
@@ -0,0 +1,86 @@
+package com.jsfcourse.calc;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+public class jsf01 {
+    private String kwota;
+    private String lata;
+    private Double raty;
+    private Double result;
+
+    @Inject
+    FacesContext ctx;
+
+    public String getKwota() {
+        return kwota;
+    }
+
+    public void setKwota(String kwota) {
+        this.kwota = kwota;
+    }
+
+    public String getLata() {
+        return lata;
+    }
+
+    public void setLata(String lata) {
+        this.lata = lata;
+    }
+
+    public Double getRaty() {
+        return raty;
+    }
+
+    public void setRaty(Double raty) {
+        this.raty = raty;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+
+    public boolean doTheMath() {
+        try {
+            double kwota = Double.parseDouble(this.kwota);
+            double lata = Double.parseDouble(this.lata);
+            double raty = this.raty;
+
+            if (raty < 2 || raty > 60) {
+                ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ilość rat musi być między 2 a 60", null));
+                return false;
+            }
+
+            result = (kwota + (kwota * (raty / 100))) / (lata * 12);
+
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Wykonano obliczenia", null));
+            return true;
+        } catch (Exception e) {
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Złe dane", null));
+            return false;
+        }
+    }
+
+    public String obliczanie() {
+        if (doTheMath()) {
+            return "showresult";
+        }
+        return null;
+    }
+
+    public String obliczanie_AJAX() {
+        if (doTheMath()) {
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Rata miesięczna: " + result, null));
+        }
+        return null;
+    }
+}
